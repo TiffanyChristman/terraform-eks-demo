f5a80c209caa4be6d020c456db81dbd265bd63a1
tiffanychristman <tiffany.christman@gmail.com>
2025-09-17 05:34:22 -0500

    feat: bootstrap repo with Terraform placeholder + CI

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "random" {}
+
+resource "random_pet" "demo" {
+  length = 2
+}
